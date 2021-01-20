90d58b845a62aee95774940f1d41e29c92028735
wellpinho <wellpinho@outlook.com>
2021-01-20 04:02:24 -0300

    Finalizado estrutura inicial do projeto

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,12 +1,25 @@
-import { application, Router } from 'express'
-
+import { Router } from 'express'
+// import productRouter from '@modules/products/routes/Products.routes'
+// import usersRouter from '@modules/users/routes/Users.routes'
+// import sessionRouter from '@modules/users/routes/SessionsRoutes'
+// import passwordRouter from '@modules/users/routes/Password.routes'
+// import profileRouter from '@modules/users/routes/Profile.routes'
+// import customersRoutes from '@modules/customers/routes/Customers.routes'
+// import orderRoutes from '@modules/orders/routes/Order.routes'
 const routes = Router()
 
 routes.get('/', (req, res) => {
-  return res.json({
-    status: '200 OK!',
-    message: 'Hello World!'
+  return res.status(200).json({
+    message: 'Hello Dev!'
   })
 })
+
+// routes.use('/products', productRouter)
+// routes.use('/users', usersRouter)
+// routes.use('/sessions', sessionRouter)
+// routes.use('/password', passwordRouter)
+// routes.use('/profile', profileRouter)
+// routes.use('/customers', customersRoutes)
+// routes.use('/orders', orderRoutes)
 
 export default routes
