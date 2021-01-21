30f9db4ea75a654b879c9d210e93d5ecf0b5dd23
wellpinho <wellpinho@outlook.com>
2021-01-21 03:25:33 -0300

    Entidade UserTokenn

---
+++ src/modules/user/typeorm/entities/UserToken.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Generated } from "typeorm";
+
+@Entity('users')
+class UserToken {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+}
+
+export default UserToken
