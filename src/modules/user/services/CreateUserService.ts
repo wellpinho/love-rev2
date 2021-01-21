8cc42b6203aae87c9e3e6edd7581eedf3550476f
wellpinho <wellpinho@outlook.com>
2021-01-21 01:29:18 -0300

    Criptografando a senha do user

--- src/modules/user/services/CreateUserService.ts
+++ src/modules/user/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from "@shared/errors/AppError";
+import { hash } from "bcryptjs";
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import { UserRepository } from "../typeorm/repositories/UseRepository";
@@ -19,10 +20,12 @@ class CreateUserService {
       throw new AppError('There is already user with this email.')
     }
 
+    const hashedPassword = await hash(password, 8)
+
     const user = userRepository.create({
       name,
       email,
-      password
+      password: hashedPassword
     })
 
     await userRepository.save(user)
