ea17c3df9b765efd3cb31084fa6928c0e178c219
wellpinho <wellpinho@outlook.com>
2021-01-21 01:41:44 -0300

    SessionsController

---
+++ src/modules/user/controller/SessionsController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express'
+import CreateSessionsService from '../services/CreateSessionsService'
+
+class SessionsController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body
+
+    const createSession = new CreateSessionsService()
+
+    const user = await createSession.execute({
+      email,
+      password
+    })
+
+    return res.json(user)
+  }
+}
+
+export default SessionsController
